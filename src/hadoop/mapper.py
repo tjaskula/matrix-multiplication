dd8da56a70a7f3a1a1f488928a75162097e56b60
Tomasz Jaskula <thomasv1000@hotmail.fr>
2018-11-07 23:14:55 +0100

    Moving spark algorithm to src/spark folder

--- src/hadoop/mapper.py
+++ src/hadoop/mapper.py
@@ -1,14 +1,20 @@
 #! /usr/bin/env python
 import sys
 
+# number of rows in A
+m = int(sys.argv[1])
+
+# number of columns in B
+p = int(sys.argv[2])
+
 for line in sys.stdin:
     line = line.strip()
     id_mat, row, col, val = line.split(",")
 
     if id_mat == "A":
-        for x in range(1, 3):
+        for x in range(p):
             print '%s %s %s %s' % (row, x, col, val)
     else:
-        for y in range(1, 3):
-            print '%s %s %s %s' % (y, col, row, val)
+        for x in range(m):
+            print '%s %s %s %s' % (x, col, row, val)
 
--- src/hadoop/reducer.py
+++ src/hadoop/reducer.py
@@ -2,37 +2,29 @@
 
 import sys
 
-cle = None
-j = None
-current_cle = None
+current_key = None
 current_j = None
-current_v = 0
-produit = 0
-somme = 0
+current_val = 0
+result = 0
 
 for line in sys.stdin:
-    sub_cle1, sub_cle2, j, val = line.strip().split()
-    cle = '%s-%s' % (sub_cle1, sub_cle2)
+    sub_key1, sub_key2, j, val = line.strip().split()
+    key = '%s-%s' % (sub_key1, sub_key2)
 
     try:
         val = int(val)
     except ValueError:
         continue
-    if (current_cle == cle) and (current_j == j):
-        produit = current_v * val
+    if (current_key == key) and (current_j == j):
+        result += current_val * val
     else:
-        if (current_cle == cle) and (current_j != j):
-            somme += produit
-            current_j = j
-            current_v = val
-        else:
-            if current_cle:
-                print(current_cle, somme + produit)
-            current_cle = cle
-            current_j = j
-            current_v = val
-            somme = 0
-            produit = 0
+        if current_key != key:
+            if current_key:
+                print(current_key, result)
+            current_key = key
+            result = 0
 
-if (current_cle == cle) and (current_j == j):
-    print(current_cle, somme + produit)
+        current_j = j
+        current_val = val
+
+print(current_key, result)
